58bb6d999304f9ea5bdcd0303a54c1dfd309d731
Aman <amandixit99@outlook.com>
2022-10-06 19:55:11 +0530

    day2 kafka listener configured

---
+++ src/main/java/com/javaguides/springboot/kafka/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.javaguides.springboot.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "javahuides",groupId = "myGroup")
+    public void consume(String mssg)
+    {
+        System.out.println("consumed mssg"+mssg);
+    }
+
+}
